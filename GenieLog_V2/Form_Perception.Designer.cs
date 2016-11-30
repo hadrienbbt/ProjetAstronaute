aa0b6fbdccc05bc27779e94cc20aabc87ad373d3
Hadrien Barbat <hbarbat@ensc.fr>
2016-11-30 12:21:23 +0100

    ajout de classe

---
+++ GenieLog_V2/Form_Perception.Designer.cs
@@ -0,0 +1,79 @@
+﻿using ProjetAstronaute;
+using System.Drawing;
+
+namespace GenieLog_V2
+{
+    partial class Form_Perception
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titre = new System.Windows.Forms.Label();
+            this.niveau = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // titre
+            // 
+            this.titre.AutoSize = true;
+            this.titre.Font = new System.Drawing.Font("Arial", 18F);
+            this.titre.Location = new System.Drawing.Point(111, 9);
+            this.titre.Name = "titre";
+            this.titre.Size = new System.Drawing.Size(227, 54);
+            this.titre.TabIndex = 0;
+            this.titre.Text = "Test Perception et \r\nmémoire associative";
+            this.titre.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // niveau
+            // 
+            this.niveau.AutoSize = true;
+            this.niveau.Font = new System.Drawing.Font("Arial", 12F);
+            this.niveau.Location = new System.Drawing.Point(192, 63);
+            this.niveau.Name = "niveau";
+            this.niveau.Size = new System.Drawing.Size(69, 18);
+            this.niveau.TabIndex = 1;
+            this.niveau.Text = "Niveau 1";
+            // 
+            // Form_Perception
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 509);
+            this.Controls.Add(this.niveau);
+            this.Controls.Add(this.titre);
+            this.Name = "Form_Perception";
+            this.Text = "Form_Perception";
+            this.Click += new System.EventHandler(this.Form_Perception_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titre;
+        private System.Windows.Forms.Label niveau;
+    }
+}
---
+++ GenieLog_V2/Form_Perception.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ProjetAstronaute;
+
+namespace GenieLog_V2
+{
+    public partial class Form_Perception : Form
+    {
+        private Graphics g;
+        private Perception data;
+        private int state;
+
+        public Form_Perception(int niveau)
+        {
+            this.data = new Perception(niveau);
+            state = 1;
+            InitializeComponent();     
+        }
+
+        public void dessinerForme(PaintEventArgs e, string forme, string couleur, int x, int y)
+        {
+
+            if (forme == "carre")
+            {
+                if (couleur == "Blue")
+                    e.Graphics.FillRectangle(Brushes.Blue, new Rectangle(x, y, 60, 60));
+                else
+                    e.Graphics.FillRectangle(Brushes.Yellow, new Rectangle(x, y, 60, 60));
+            }
+            else
+            {
+                if (couleur == "Blue")
+                    e.Graphics.FillEllipse(Brushes.Blue, new Rectangle(x, y, 60, 60));
+                else
+                    e.Graphics.FillEllipse(Brushes.Yellow, new Rectangle(x, y, 60, 60));
+            }
+        }
+
+        public void dessinerLettre(PaintEventArgs e, char lettre, int x, int y)
+        {
+            // Create font and brush.
+            Font drawFont = new Font("Arial", 16);
+            SolidBrush drawBrush = new SolidBrush(Color.Black);
+
+            // Create point for upper-left corner of drawing.
+            PointF drawPoint = new PointF(x, y);
+
+            // Draw string to screen.
+            e.Graphics.DrawString(lettre.ToString().ToUpper(), drawFont, drawBrush, drawPoint);
+        }
+
+        public void dessinerChiffre(PaintEventArgs e, int chiffre, int x, int y)
+        {
+            // Create font and brush.
+            Font drawFont = new Font("Arial", 16);
+            SolidBrush drawBrush = new SolidBrush(Color.Black);
+
+            // Create point for upper-left corner of drawing.
+            PointF drawPoint = new PointF(x, y);
+
+            // Draw string to screen.
+            e.Graphics.DrawString(chiffre.ToString(), drawFont, drawBrush, drawPoint);
+        }
+
+        public void dessinerInput(PaintEventArgs e, Forme f, int x, int y)
+        {
+            dessinerLettre(e, f.getLettre(), x, y);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            // Si on en est à l'étape des formes, on les dessine
+            switch (this.state) {
+                case 1 :
+                    int x = 80,
+                        y = 160;
+                    e.Graphics.DrawString(data.getRegle(), new Font("Arial", 13), new SolidBrush(Color.Black), new PointF(x-30, y-60));
+                    foreach (Forme f in data.getFormes())
+                    {
+                        dessinerLettre(e, f.getLettre(), x+20, y-25);
+                        dessinerForme(e, f.getType(), f.getCouleur(), x, y);
+                        dessinerChiffre(e, f.getChiffre(), x + 20, y + 20);
+                        if (x >= 250) {
+                            x = 80;
+                            y += 100;
+                        } else {
+                            x += 80;
+                        }
+                    }
+                    break;
+                case 2 :
+                    x = 200;
+                    y = 160;
+                    foreach (Forme f in data.getFormes()) {
+                        if (f.getCouleur() == data.getCouleurRef() && f.getType() == data.getFormeRef()) {
+                            dessinerInput(e, f, x, y);
+                        }
+                        if (x >= 250){
+                            x = 80;
+                            y += 100;
+                        } else {
+                            x += 80;
+                        }
+                    }
+                    break;
+            }
+            base.OnPaint(e);
+        }
+
+        private void Form_Perception_Click(object sender, EventArgs e)
+        {
+            data = new Perception(data.getNiveau());
+            this.state = 2;
+            Refresh();
+        }
+    }
+}
